883c10ed78c22c5d6ab5c5ee8aa1b718c1ac4bab
SyedaTazmeenZahra <sofiasareem07@gmail.com>
2024-11-03 10:56:40 +0500

    This is my PF LAB 06

---
+++ Task-05.2.cpp
@@ -0,0 +1,19 @@
+//Seating arrangement in a theater using nested while loop
+#include <iostream>
+using namespace std;
+int main() {
+    int i = 1;
+    while (i <= 5)
+    {
+        cout << "Row " << i << ": " << endl << "\t";
+        int j = 1;
+        while (j <= 10)
+        {
+            cout << "Seat " << j << " ; ";
+            j ++;
+        }
+        cout << endl;
+        i ++;
+    }
+    return 0;
+}
